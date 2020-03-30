14e55349d9481b0e74d2483ecf44e99602da2dcb
urvashigupta7 <urvashi072000@gmail.com>
2020-03-30 20:07:26 +0530

    finally final

--- src/Components/pages/login.js
+++ src/Components/pages/login.js
@@ -1,6 +1,13 @@
 import React, { useContext, useEffect } from 'react';
 import AuthContext from '../../Context/auth/authContext';
 import codechefIcon from './codecheficon2.png';
+let redirect_uri='';
+if(process.env.NODE_ENV!=='production'){
+	    redirect_uri=process.env.REACT_APP_REDIRECT_URI;
+}
+else{
+    redirect_uri=process.env.REDIRECT_URI
+}
 const Login = (props) => {
     const authContext = useContext(AuthContext)
     useEffect(() => {
@@ -9,7 +16,7 @@ const Login = (props) => {
         }
     }, [props.history])
     const onClick = () => {
-        window.location.href = 'https://api.codechef.com/oauth/authorize?response_type=code&client_id=e5e6c5bae70021cf68095a49e9708f09&state=xyz&redirect_uri=http://localhost:3000/';
+        window.location.href = 'https://api.codechef.com/oauth/authorize?response_type=code&client_id=e5e6c5bae70021cf68095a49e9708f09&state=xyz&redirect_uri='+redirect_uri;
         authContext.setRequestLogin();
     }
     return (
